5b31e65f101e7cbcddc08826f5edb6ba40e84633
a495433 <a495433@qq.com>
2017-10-11 10:20:56 +0800

    引入了一个封装好的webview
    引入了一个封装好的webview

--- MyApplication/mylibrary/src/main/java/com/example/mylibrary/BaseActivity.kt
+++ MyApplication/mylibrary/src/main/java/com/example/mylibrary/BaseActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mylibrary
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.just.library.AgentWeb
 
 /**
  * Created by finfo on 10/10/17.
  */
 open class BaseActivity :AppCompatActivity() {
-
-
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
